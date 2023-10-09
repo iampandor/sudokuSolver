5de75c2e080fae594e20763af13bd1ae4e379f6b
Saul Martinez <i.saulmar@gmail.com>
2023-10-08 19:10:43 -0600

    Added control to visualize sudoku line

--- main.cpp
+++ main.cpp
@@ -2,21 +2,39 @@
 
 int main(){
     std::cout << "Please write the values of the sudoku board: ";
-    short* sudokuLine=new short[3];
+    bool* sudokuLine=new bool[9];
     
     short** sudokuBoard = new short*[3];
     
     for(int i=0;i<3;i++){
-    	sudokuBoard[i]=new short[3];//1
-    	for(int j=0;j<3;j++){
-    		std::cin>>sudokuBoard[i][j];
-    	}
+        sudokuBoard[i]=new short[3];
+        for(int j=0;j<3;j++){
+            std::cin >> sudokuBoard[i][j];
+        }
     }
+
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            std::cout<<sudokuBoard[i][j]<<"\t";
+            char value = sudokuBoard[i][j]==0 ? '*' : sudokuBoard[i][j]+48;
+            std::cout << value << "\t";
         }
-        std::cout<<std::endl;
+        std::cout << std::endl;
+    }
+
+    for (short i=1;i<10;i++){
+        sudokuLine[i-1] = false;
+    }
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(sudokuBoard[i][j]!=0){
+                sudokuLine[sudokuBoard[i][j]-1] = true;
+            }
+        }
+    }
+
+    for (short i=1;i<10;i++){
+        std::cout<<sudokuLine[i]<<std::endl;
     }
 }
 
