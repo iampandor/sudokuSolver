7853ee032c7b12e37fd8b5cab14f443c76db1921
Saul Martinez <i.saulmar@gmail.com>
2023-10-09 20:35:50 -0600

    modularize code

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Please write the values of the sudoku board: ";
-    bool* sudokuRow=new bool[9];
-    bool* sudokuCol=new bool[9];
-    bool* sudokuSqr=new bool[9];
-    
-    short** sudokuSquare = new short*[3];
-    
+#define SQUARE_SIZE
+
+bool* sudokuRow=new bool[9];
+bool* sudokuCol=new bool[9];
+bool* sudokuSqr=new bool[9];
+
+void initializeSquare(short** sudokuSquare){
     for(int i=0;i<3;i++){
         sudokuSquare[i]=new short[3];
         for(int j=0;j<3;j++){
             std::cin >> sudokuSquare[i][j];
         }
     }
+}
 
+void printSquare(short** sudokuSquare){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             char value = sudokuSquare[i][j]==0 ? '*' : sudokuSquare[i][j]+48;
@@ -22,37 +23,74 @@ int main(){
         }
         std::cout << std::endl;
     }
+}
 
-    for (short i=1;i<10;i++){
-        sudokuSqr[i-1] = false;
-    }
-
+void checkSquare(short** sudokuSquare){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             if(sudokuSquare[i][j]!=0){
                 sudokuSqr[sudokuSquare[i][j]-1] = true;
             }
         }
+    }
+}
+
+void checkRow(short** sudokuSquare){
+    for(int i=0;i<3;i++){
         if(sudokuSquare[0][i]!=0){
             sudokuCol[sudokuSquare[0][i]-1] = true;
         }
+    }
+}
+
+void checkColumn(short** sudokuSquare){
+    for(int i=0;i<3;i++){
         if(sudokuSquare[0][i]!=0){
             sudokuRow[sudokuSquare[i][0]-1] = true;
         }
     }
-    std::cout<<"Cuadrado: ";
-    for (short i=0;i<9;i++){
-        std::cout<<sudokuSqr[i];
-    }
-    std::cout<<std::endl<<"Columnas: ";
-    for (short i=0;i<9;i++){
-        std::cout<<sudokuCol[i];
-    }
-    std::cout<<std::endl<<"Filas: ";
+}
+
+void printRowStats(){
+    std::cout<<"Filas: ";
     for (short i=0;i<9;i++){
         std::cout<<sudokuRow[i];
     }
     std::cout<<std::endl;
+}
+
+void printColStats(){
+    std::cout<<"Columnas: ";
+    for (short i=0;i<9;i++){
+        std::cout<<sudokuCol[i];
+    }
+    std::cout<<std::endl;
+}
+
+void printSqrStats(){
+    std::cout<<"Cuadrado: ";
+    for (short i=0;i<9;i++){
+        std::cout<<sudokuSqr[i];
+    }
+    std::cout<<std::endl;
+}
+
+int main(){
+    std::cout << "Please write the values of the sudoku board: ";
+    
+    short** sudokuSquare = new short*[3];
+    
+    initializeSquare(sudokuSquare);
+    
+    printSquare(sudokuSquare);
+
+    checkSquare(sudokuSquare);
+    checkRow(sudokuSquare);
+    checkColumn(sudokuSquare);
+    
+    printSqrStats();
+    printColStats();
+    printRowStats();
 }
 
 /* Sudoku sample
